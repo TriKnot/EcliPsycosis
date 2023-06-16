451f41005ff705a59cbea4ea1fa10a1de5274f27
TriKnot <johan.brandt91@gmail.com>
2023-06-16 15:27:40 +0200

    Portal checks for player when no enemies left

--- Source/Roguelike/Public/Subsystems/WorldStateSubSystem.h
+++ Source/Roguelike/Public/Subsystems/WorldStateSubSystem.h
@@ -6,6 +6,9 @@
 #include "Subsystems/WorldSubsystem.h"
 #include "WorldStateSubSystem.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnActiveEnemyCountChanged);
+
+
 class AEnemyCharacter;
 /**
  * 
@@ -16,18 +19,33 @@ class ROGUELIKE_API UWorldStateSubSystem : public UWorldSubsystem
 	GENERATED_BODY()
 
 public:
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+	FOnActiveEnemyCountChanged OnActiveEnemyCountChanged;
 	
 	UFUNCTION(BlueprintCallable, Category = "WorldStateSubSystem")
 	FORCEINLINE TArray<AEnemyCharacter*> GetActiveEnemies() const {	return ActiveEnemies;	}
 	
 	UFUNCTION(BlueprintCallable, Category = "WorldStateSubSystem")
-	FORCEINLINE void AddActiveEnemy(AEnemyCharacter* Enemy)	{ ActiveEnemies.Add(Enemy);	}
+	FORCEINLINE void AddActiveEnemy(AEnemyCharacter* Enemy)
+	{
+		OnActiveEnemyCountChanged.Broadcast();
+		ActiveEnemies.Add(Enemy);
+	}
 	
 	UFUNCTION(BlueprintCallable, Category = "WorldStateSubSystem")
-	FORCEINLINE void RemoveActiveEnemy(AEnemyCharacter* Enemy) { ActiveEnemies.Remove(Enemy); }
+	FORCEINLINE void RemoveActiveEnemy(AEnemyCharacter* Enemy)
+	{
+		OnActiveEnemyCountChanged.Broadcast();
+		ActiveEnemies.Remove(Enemy);
+	}
 
 	UFUNCTION(BlueprintCallable, Category = "WorldStateSubSystem")
-	FORCEINLINE void ClearActiveEnemies() { ActiveEnemies.Empty(); }
+	FORCEINLINE void ClearActiveEnemies()
+	{
+		OnActiveEnemyCountChanged.Broadcast();
+		ActiveEnemies.Empty();
+	}
 
 	UFUNCTION(BlueprintCallable , Category = "WorldStateSubSystem")
 	TArray<AEnemyCharacter*> GetActiveEnemiesWithingDistanceFromPoint(FVector Point, float Distance) const;
